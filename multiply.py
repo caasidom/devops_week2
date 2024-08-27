cf24e6a5574add9f7f2f1b66b70e349b06d18ebe
Antigs <davidantigha7@gmail.com>
2024-08-27 13:29:45 +0100

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,8 +1,5 @@
 # A function that multiply two numbers and get the result
 def get_multiply(num1=20, num2=5, multiply=20*5):
-
-
-    
     print('find the multiple of two values')
     print(num1, "*", num2, "=", multiply)  
 get_multiply()
